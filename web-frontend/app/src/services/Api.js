6232e8d92117f08d58e87337798de07b71ec58d0
KaiL4eK <darklexi494@gmail.com>
2020-04-08 11:35:32 +0300

    Added namespace to requests

--- web-frontend/app/src/services/Api.js
+++ web-frontend/app/src/services/Api.js
@@ -5,7 +5,7 @@ console.log(process.env.API_URL)
 
 export default() => {
     return axios.create({
-        baseURL: 'http://127.0.0.1:5000',
+        baseURL: process.env.API_URL + '/api/v1',
         withCredentials: false,
         headers: {
             'Accept': 'application/json',
