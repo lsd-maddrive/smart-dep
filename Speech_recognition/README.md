30cc0df14b762991e87caf5f23d856a575ac95cc
dohako <dohako@inbox.ru>
2020-04-08 21:33:35 +0300

    Add new folder

---
+++ Speech_recognition/README.md
@@ -0,0 +1,9 @@
+# smart-dep
+Smart Department project
+
+# Feature/5
+
+## Здесь будут тестироваться различные технологии распознавания речи
+
+в папке pocketsphynx_testing будет тестироваться технлология, использующая pocketsphynx
+
---
+++ Speech_recognition/pocketsphynx_testing/computer4.py
@@ -0,0 +1,23 @@
+import os
+from pocketsphinx import LiveSpeech, get_model_path
+
+model_path = get_model_path()
+
+speech = LiveSpeech(
+    verbose=False,
+    sampling_rate=16000,
+    buffer_size=2048,
+    no_search=False,
+    full_utt=False,
+    hmm=os.path.join(model_path, 'zero_ru.cd_cont_4000'),
+    lm=os.path.join(model_path, 'ru.lm'),
+    keyphrase='компьютер',
+    kws_threshold=1e-20,
+    dic=os.path.join(model_path, 'ru.dic')
+)
+
+
+print("Say something!")
+
+for phrase in speech:
+    print(phrase.segments(detailed=True))
