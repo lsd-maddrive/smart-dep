899e011bde1c23da72cdfa56c9eed7ca73718578
KaiL4eK <darklexi494@gmail.com>
2020-04-09 20:10:00 +0300

    Added config, fixed web issue

---
+++ api-server/config/dev/config.py
@@ -0,0 +1,3 @@
+DEBUG = True
+TESTING = False
+DATABASE_URI = 'postgresql+psycopg2://admin:admin@127.0.0.1:5432/smart_dep'
---
+++ api-server/config/test/config.py
@@ -0,0 +1,3 @@
+DEBUG = False
+TESTING = True
+DATABASE_URI = 'postgresql+psycopg2://admin:admin@127.0.0.1:5432/smart_dep'
