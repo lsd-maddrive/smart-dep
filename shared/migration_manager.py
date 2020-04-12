f18ba4e2297a221fdb15eee11787efe67ea28c71
Lena <seryhelena@gmail.com>
2020-04-12 21:05:26 +0300

    SMART_ENV - not working

--- shared/migration_manager.py
+++ shared/migration_manager.py
@@ -5,17 +5,12 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 
-# import environment_manager.py
 from models.table_models import db
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
-app.config["DEBUG"] = os.getenv("DEBUG")
+app.config.from_object(os.environ.get('CONFIG_OBJ'))
 
-print(os.getenv("SQLALCHEMY_DATABASE_URI"))
-print(os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS"))
-print(os.getenv("DEBUG"))
+print(os.environ.get('CONFIG_OBJ'))
 
 db.init_app(app)
 
