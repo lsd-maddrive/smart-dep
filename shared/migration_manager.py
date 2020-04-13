20a7ff47e3c65fc0676b9512c0c67b7591eebbe4
Lena <seryhelena@gmail.com>
2020-04-13 21:43:33 +0300

    fix migrate_manager, remove .env.test, fixt tests with DB

--- shared/migration_manager.py
+++ shared/migration_manager.py
@@ -9,15 +9,8 @@ from models.table_models import db
 
 app = Flask(__name__)
 
-env_mode = os.environ.get('SMART_ENV')
-if env_mode == 'test':
-    config_path = 'config/test/config.py'
-elif env_mode == 'prod':
-    config_path = 'config/prod/config.py'
-elif env_mode == 'dev':
-    config_path = 'config/dev/config.py'
-else: # in other cases - dev mode 
-    config_path = 'config/dev/config.py'
+env_mode = os.getenv('SMART_ENV', 'dev')
+config_path = f'config/{env_mode}/config.py'
 
 app.config.from_pyfile(config_path)
 
