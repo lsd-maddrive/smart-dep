8b9522e974dea7127c4da7b410758fc7e3006d25
Lena <seryhelena@gmail.com>
2020-04-26 22:47:07 +0300

    Buffer + timeout for states

--- logger/logger.py
+++ logger/logger.py
@@ -5,11 +5,11 @@ import os
 import sys 
 sys.path.append("..")
 
-from flask_sqlalchemy import SQLAlchemy
 import pika 
 from pprint import pprint
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+import threading
 import yaml 
 
 from shared.models.table_models import metadata, Commands, Params, States 
@@ -87,12 +87,30 @@ class StateLogger(Logger):
         )
 
         self.state_buffer = []
-        # embedded autoincrement can not track 
-        # current changes in DB and use cached values 
-        self.cb_count = self.session.query(States).count()
         self.BUFFER_LIMIT = 10
+        self.TIMEOUT_S = 3.0 
+
+        self.lock = threading.Lock()
+        
     
+    def send_package_to_db(self):
+        if len(self.state_buffer) == 0:
+            return 
+
+        self.lock.acquire()
+        # print(f"Func: {len(self.state_buffer)}")
+        self.session.bulk_save_objects(
+            objects=self.state_buffer
+        )
+        self.session.commit() 
+        self.state_buffer = []
+        self.timer.cancel() 
+
+        self.lock.release()
+
+
     def callback(self, ch, method, properties, body):
+
         state_json = json.loads(body.decode('utf-8'))
 
         if 'timestamp' not in state_json:
@@ -100,31 +118,27 @@ class StateLogger(Logger):
         else:
             timestamp = state_json['timestamp']
         
-        self.cb_count += 1
-        next_id = self.cb_count
-
         new_state = States(
-            id=next_id,
             timestamp=timestamp,
             state=state_json['state'],
             device_id=state_json['device_id'], 
             place_id=state_json['place_id'],
             type=state_json['type']
         )
-       
-        print(f"Length of buffer: {len(self.state_buffer)}, cb_c: {self.cb_count}")
+
+        if len(self.state_buffer) == 0:
+            self.timer = threading.Timer(
+                interval=self.TIMEOUT_S,
+                function=self.send_package_to_db
+            )
+            self.timer.start() 
+
+        # print(f"Length of buffer: {len(self.state_buffer)}")
         self.state_buffer.append(new_state)
 
         if len(self.state_buffer) >= self.BUFFER_LIMIT:
-            self.session.bulk_save_objects(
-                objects=self.state_buffer
-            )
-            self.session.commit()
-            self.state_buffer = []
+            self.send_package_to_db() 
         
-        
-
-
 class ConfigLogger(Logger):
     def __init__(self, config, session):
         self.exchange_name = "configurations"
@@ -208,8 +222,9 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args() 
-
-    with open(args.config) as cfg:
+    # INPUT:
+    #   python3 logger.py -c dev/config.yml
+    with open('config/' + args.config) as cfg:
         total_cfg = yaml.safe_load(cfg)
 
     logger_cfg = total_cfg['logger']
