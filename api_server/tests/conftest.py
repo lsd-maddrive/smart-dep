91ba060fe54ff899e9dbaf5c0b9e77cdfc039a18
Lena <seryhelena@gmail.com>
2020-05-25 14:27:49 +0300

    fixture for db - work

--- api_server/tests/conftest.py
+++ api_server/tests/conftest.py
@@ -1,39 +1,44 @@
-import os
-# import sys 
-# sys.path.append("../")
-import tempfile
+from datetime import datetime
+import logging
 
 import pytest
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import testing.postgresql
 
 from db.models import * 
 
-# from api_server.app import app, db  
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d/%H:%M:%S')
+logger = logging.getLogger(__name__)
 
-# @pytest.fixture(scope='session')
-# def client():
-#     db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-#     app.config['TESTING'] = True
-
-#     with app.test_client() as client:
-#         with app.app_context():
-#     #         flaskr.init_db() # ???????????????
-#             pass
-#         yield client
-
-#     os.close(db_fd)
-#     os.unlink(app.config['DATABASE'])
-
-
-# or maybe scope='module' ??? 
-# session - this fixture is visiable for every tests
-# module - invoked once per module 
 @pytest.fixture(scope='session')
-def timescaleDB():
-    db = testing.postgresql.Postgresql()
-    engine = create_engine(db.url())
+def timescaleDB(request):
+    test_db = testing.postgresql.Postgresql()
+    engine = create_engine(test_db.url())
+    logger.debug(f"Engine is creates: {test_db.url()}")
     Model.metadata.create_all(engine)
+    logger.debug('Create all models - done ')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    logger.debug('session - done')
+    
+    test_state = States(
+        timestamp=datetime.now(), 
+        state= {'enabled': False}, 
+        device_id='11:11:11:11:11:11',
+        place_id='8201',
+        type='light'
+    )
+    logger.debug(f"Test row: {test_state}")
+    session.add(test_state)
+    logger.debug("Test row - added")
+    session.commit()
+    logger.debug("Test row - commited")
 
-    # db = testing.postgresql.PostgresqlFactory(cache_initialized_db=True)
-   
+    def resource_teardown():
+        logger.debug("resource_teardown")
+        session.close()
+        test_db.stop()
+    request.addfinalizer(resource_teardown)
+
+    return session
--- api_server/tests/test_flask.py
+++ api_server/tests/test_flask.py
@@ -1,48 +1,19 @@
-import os
-import uuid
-import tempfile
+import logging
 
 import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-# from sqlalchemy_utils import create_database, drop_database
-# from yarl import URL
-import testing.postgresql
 
 from db.models import * 
-# from api_server.app import app 
 
-@pytest.fixture(scope='session')
-def timescaleDB():
-    db = testing.postgresql.Postgresql()
-    engine = create_engine(db.url())
-    session = sessionmaker(bind=engine)
-    Model.metadata.create_all(engine)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d/%H:%M:%S')
+logger = logging.getLogger(__name__)
 
-    tmp = session.query(States).limit(2)
+def test_db(timescaleDB):
+    tmp = timescaleDB.query(States).all()
+    logger.debug(f"Test query: {tmp}")
+
     for t in tmp:
         print("FIXTURE ")
+        print(f"{t.state}")
         print(f"{t}")
-    
-    return db.url()
-    
-
-def test_db():
-
-    db = testing.postgresql.Postgresql()
-    engine = create_engine(db.url())
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Model.metadata.create_all(engine)
-
-    tmp = session.query(States).limit(2)
-    for t in tmp:
-        print("FIXTURE ")
-        print(f"{t}")
-
-    # tmp = timescaleDB.query(States).limit(2)
-    # for t in tmp:
-    #     print("FIXTURE ")
-    #     print(f"{t}")
 
     assert True
