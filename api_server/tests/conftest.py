ae31393529341a6dc838222ab300ca080203ad1a
Lena <seryhelena@gmail.com>
2020-05-27 22:40:09 +0300

    socketio testing - fail

--- api_server/tests/conftest.py
+++ api_server/tests/conftest.py
@@ -1,14 +1,19 @@
+import os
 from datetime import datetime
 import logging
 
 # from flask_sqlalchemy import SQLAlchemy
+from flask_restplus import Api
+from flask_cors import CORS
 from pprint import pformat
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import testing.postgresql
 
+from api_server.api_v1 import api as ns 
 from api_server.api_func import create_app
+from api_server.sockets import socketio
 from db.models import Model, States 
 
 from db.database import db 
@@ -53,7 +58,7 @@ def timescaleDB(request, test_db):
             states.append(
                 States(
                     timestamp=datetime.now(), 
-                    state= {'enable': False}, 
+                    state= {'enabled': False}, 
                     device_id=device, 
                     place_id='8201', 
                     type=type_
@@ -69,18 +74,15 @@ def timescaleDB(request, test_db):
     session.commit() 
     
     def resource_teardown():
-        logger.debug("Resource teardown!")
+        logger.debug("Database Resource teardown!")
         session.close()
         test_db.stop()
     request.addfinalizer(resource_teardown)
 
     return session
 
-
-
 @pytest.fixture(scope='session')
-def client(test_db):
-    logger.debug('Inside client fixture')
+def flask_app(test_db, timescaleDB):
     app = create_app(test_config=True)
 
     app.config['DATABASE'] = test_db.url()
@@ -88,12 +90,30 @@ def client(test_db):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config['FLASK_DEBUG'] = False
     app.config['TESTING'] = True
+    app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
 
     logger.debug(f'Test App DB: {app.config["SQLALCHEMY_DATABASE_URI"]}')
-    
     db.init_app(app)
-    # with app.test_client() as client:
-    #     with app.app_context():
+
+    socketio.init_app(app)
+    api = Api(app)
+    api.add_namespace(ns)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     return app
+
+
+@pytest.fixture(scope='session')
+def client(flask_app):
+    with flask_app.test_client() as client:
+        yield client
+
+
+@pytest.fixture(scope='session')
+def sio_client(flask_app, client):
+    sio_test_client = socketio.test_client(
+        flask_app, flask_test_client=client
+    )
+
+    return sio_test_client
             
--- api_server/tests/test_flask.py
+++ api_server/tests/test_flask.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import pytest
@@ -7,7 +8,65 @@ from db.models import *
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d/%H:%M:%S')
 logger = logging.getLogger(__name__)
 
-def test_db(client):
+def test_powers_request(timescaleDB, client):
+    _ = timescaleDB.query(States.device_id, States.state, States.type). \
+                    filter(States.type == 'power'). \
+                    order_by(States.device_id)
+    # to store expected rows 
+    right_states = []
+    for right_state in _:
+        right_states.append(
+            {
+            'state': right_state.state,
+            'device_id': right_state.device_id,
+            'type': right_state.type
+            }
+        )
     
+    rv = client.get('http://localhost:5000/api/v1/place/8201/powers')
+    # TODO: add message with details if test failed 
+    assert right_states == json.loads(rv.data)
 
+def test_lights_request(timescaleDB, client):
+    _ = timescaleDB.query(States.device_id, States.state, States.type). \
+                    filter(States.type == 'light'). \
+                    order_by(States.device_id)
+    # to store expected rows 
+    right_states = []
+    for right_state in _:
+        right_states.append(
+            {
+            'state': right_state.state,
+            'device_id': right_state.device_id,
+            'type': right_state.type
+            }
+        )
+    
+    rv = client.get('http://localhost:5000/api/v1/place/8201/lights')
+    # TODO: add message with details if test failed 
+    assert right_states == json.loads(rv.data)
+
+def test_place_request(timescaleDB, client):
+    right_states = []
+    right_states.append(
+        {
+            'id': '8201',
+            'name': '8201',
+        }
+    )
+    rv = client.get('http://localhost:5000/api/v1/place')
+    # TODO: add message with details if test failed 
+    assert right_states == json.loads(rv.data)
+
+def test_socketio_client_is_connected(sio_client):
+    assert sio_client.is_connected()
+
+def test_socketio(sio_client):
+    # cgf = {
+    #     'place_id': '8201',
+    #     'period': '5'
+    # }
+    # sio_client.emit('start_states', cgf)
+    # sio_client.disconnect()
+    
     assert True
