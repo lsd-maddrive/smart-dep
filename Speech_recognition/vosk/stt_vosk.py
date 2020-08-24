3142a6f3e6a816288d570b4dcadbc78f4048885c
Dohako <dohako@inbox.ru>
2020-08-24 22:39:39 +0100

    new tests

--- Speech_recognition/vosk/stt_vosk.py
+++ Speech_recognition/vosk/stt_vosk.py
@@ -44,8 +44,8 @@ def params_checking():
 def activate_vosk():
     if params_checking() is False:
         quit()
-    #model = Model("model-ru")
-    #rec = KaldiRecognizer(model, 48000) # 16000
+    model = Model("model-ru")
+    rec = KaldiRecognizer(model, 48000) # 16000
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paInt16,
                     channels=1,
@@ -54,7 +54,7 @@ def activate_vosk():
                     frames_per_buffer=30000) # 30000
     stream.start_stream()
     
-    return stream, p#, rec, p, model
+    return stream, rec, p, model
 
 #
 engine = pyttsx3.init()
@@ -71,14 +71,63 @@ while True:
     s = {}
     #
     cycle_trigger = True
-    stream, p = activate_vosk()
-    time.sleep(3)
-    data = stream.read(1024,exception_on_overflow = False)
-    
-    
+    stream, rec, p, model = activate_vosk()
+    if stream is False:
+        quit
+
+    while cycle_trigger:
+        if int(time.time()) > checking_time + 5:
+            checking_time = int(time.time())
+            if params_checking() is False:
+                cycle_trigger = False
+        data = stream.read(1024,exception_on_overflow = False) # было 5000
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            first_res = rec.Result()
+            if len(first_res) > 17:
+                print(datetime.datetime.now().strftime("%H:%M:%S"))
+                params_checking()
+                print(first_res)
+                if "комп" in first_res or "воск" in first_res:
+                    print("FOUND KW")
+                    time_start = int(time.time())
+                    kws = True
+                time_now = int(time.time())
+                if kws == True and time_now - time_start < 10:
+                    if "свет" in first_res:
+                        if "включ" in first_res:
+                            engine.say("Turning on the light")
+                            engine.runAndWait()
+                            turn_on_light()
+                            kws = False
+                        elif "выключ" in first_res:
+                            engine.say("Turning off the light")
+                            engine.runAndWait()
+                            turn_off_light()
+                            kws = False
+                    elif "увеличить скорость речи" in first_res:
+                        engine.say("Speed up")
+                        engine.runAndWait()
+                    elif "уменьшить скорость речи" in first_res:
+                        engine.say("Speed down")
+                        engine.runAndWait()
+                    elif "врем" in first_res:
+                        now_time = datetime.datetime.now() + datetime.timedelta(hours=2)
+                        engine.say(f"the time is {now_time.strftime('%H %M')}")
+                        engine.runAndWait()
+                    else:
+                        engine.say(random.choice(hello_list))
+                        engine.runAndWait()
+                        time_start = int(time.time())
+                if time_now - time_start > 15:
+                    kws = False
+    engine.say("I have to reboot recognizer")
+    engine.runAndWait()
+    rec.FinalResult()
     stream.close()
     p.terminate()
-    print(stream)
+    #print(stream)
     rec = None
     data = None
     model = None
